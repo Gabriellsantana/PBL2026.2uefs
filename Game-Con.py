6c52cae31c83d293bf9923dcd3b7c1634aaa49a0
Gabriel <gabrielnascimentosantana.ba@gmail.com>
2026-08-23 22:51:59 -0300

    criando exe.executavel

---
+++ Game-Con.py
@@ -0,0 +1,15 @@
+import os
+#Variaveis
+funcionario:str;
+kids: int;
+adultos:int;
+universitystudent:int;
+totalespaco:int
+
+#menu inicial
+
+print("Fazer login");
+print("Usuario");
+funcionario = input();
+os.system("cls") 
+print(funcionario);
