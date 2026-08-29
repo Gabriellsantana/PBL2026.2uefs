a3fe4856837339a7616f6e2e7b60d7994026bde5
Gabriel <gabrielnascimentosantana.ba@gmail.com>
2026-08-29 13:48:59 -0300

    implematção do menu total3.0

--- Game-Con.py
+++ Game-Con.py
@@ -1,44 +1,82 @@
-import os 
+import os #importando bibiotleca para cls
+
 #Variaveis
 funcionario:str;
 kids:int;
 adultos:int;
 jovens:int;
 universitystudent:int;
-totalespaco:int;
+lotacaoMax:int;
 idade:int;
+senha:str;
+contLotacao:int;
+contLotacao=0;
+#menu inicial
+print("-------------Bem Vindo Organização do Cuca-------------");
+print("Infomer Capacidade máxima: ",end="");
+lotacaoMax = int(input());
+senha=input("Crie um senha para o dia: ");
 
-#menu inicial (protopico de ideia)
-print("Infomer lotação maxima do CUCA");
-totalespaco = input();
+  
 os.system("cls") #limpa tela 
 
-print("-------------Entradas Visitante--------------");
-print("");
-print("Nome: ",end="");
-nome = input();
-print("idade: ",end="");
-idade = int(input());
-os.system("cls") 
+opcao =""
+while opcao!= "2": #laço principal de repetição do menu
+    print("-------------Entradas Visitante--------------");
+    print("[1]Vender");
+    print("[2]Encerra\n");
+    opcao =input()
+    os.system("cls") 
 
-if idade >= 1 and idade <= 9:
-    print("\nCategoria:Crianças");
-    print("Valor:Gratis");
-    kids=+1;
+    match opcao:
+     case "1":
+        if contLotacao <= lotacaoMax:#Inicio controle para lotação maxima do espaço
+           print("Idade do visitante: ",end="");
+           idade = int(input());
 
-if idade >=10 and idade <=17:
-    print("\nCategoria:Adolecentes");
-    print("Valor:$15,00");
-    jovens=+1;
+           if idade >=1 and idade <=9:
+              print("INGRESSO VENDIDO");
+              print("Categira:Kids");
+              print("Valor:Grâtis\n");
+              contLotacao+=1;
+              print("ingressos vendidos:%d"%contLotacao);
+              print("Capacidade local:%d"%lotacaoMax);
+           
+           elif idade >=10 and idade <=17:  
+              print("INGRESSO VENDIDO");
+              print("Categoria:Adolecente");
+              print("Valor:15$\n");
+              contLotacao+=1
 
-if idade >=18:
-    print("\nCategoria:Adultos");
-    print("Valor:$40,00\n\n");
-    desconto=int(input("Desconto:[1]Sim  [2]não: "));
-    if desconto ==1:#PRECISA FZR VERIFICAÇÃO DA MATRICULA AINDA
-         print("\nCategoria:Adultos/Estudande Universiatrio");
-         print("Valor:$20,00");
-    else:
-        print("\nCategoria:Adultos");
-        print("Valor:$40,00");
+           elif idade >=18:
+              print("Estudande Universitario Uefs:")  
+              uefs=int(input("[1]sim----[2]Não"));
+              if uefs ==1:# controle estudandte
+                 print("INGRESSSO VENDIDO");
+                 print("Categoria:Estudante");
+                 print("Valor:20$\n");
+                 contLotacao+=1
+                 print("ingressos vendidos:%d"%contLotacao);
+                 print("Capacidade local:%d"%lotacaoMax);
+              else:#fim controle estudande
+                 print("INGRESSO VENDIDO");
+                 print("Categoria:Adulto");
+                 print("Valor:40$\n\n");
+                 contLotacao+=1
+                 print("ingressos vendidos:%d"%contLotacao);
+                 print("Capacidade local:%d"%lotacaoMax);
+               
 
+        else:#fim Lotação maxima
+            print("Lotação Maxima Atingida Obrigado por Hoje :)\n");
+     case "2":
+          confirmaSenha=input("Confirma Senha:");
+          if confirmaSenha == senha:
+             print("$--Caixa Encerrado--$\n");
+          else:
+             print("Senha Invalida Contantar Administrador\n");
+             opcao ="" #limpei a varivel do meu laço para não ecerra quando adm erra senha 
+     case _:#outro caso  
+          print("[#### OPÇÃO NÃO ENCONTRADA ERRO 404 ######]");
+
+         
