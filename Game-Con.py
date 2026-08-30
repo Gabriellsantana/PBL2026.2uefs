5d2c04ed8a31d02834343fca28755cd10ee0c35a
Gabriel <gabrielnascimentosantana.ba@gmail.com>
2026-08-30 10:50:16 -0300

    teste finais

--- Game-Con.py
+++ Game-Con.py
@@ -35,7 +35,7 @@ while opcao!= "2": #laço principal de repetição do menu
 
     match opcao:
      case "1":
-        if contLotacao <= lotacaoMax:#!!!!!!!!!!!!!inicio!!!!!!!!!!!!!!!!!!!!!!!!
+        if contLotacao < lotacaoMax:#!!!!!!!!!!!!!inicio!!!!!!!!!!!!!!!!!!!!!!!!
            print("Idade do visitante: ",end="");
            idade = int(input());
 
@@ -51,17 +51,18 @@ while opcao!= "2": #laço principal de repetição do menu
            
            elif idade >=10 and idade <=17:#//////////ADOLECENTE//////////////////  
               #opreçãofinceira
-              pagamento=float(input("Digite o valor pago $:"));
+              print("\nCATEGORIA:JOVENS:PREÇO:R$15,00\n");
+              pagamento=float(input("Digite o valor pago $:").replace(",","."));
               if pagamento >= 15.00:#validação pagamento menores que valor do ingresso
                troco = pagamento-15.00;
-               print("INGRESSO VENDIDO");
+               print("\nINGRESSO VENDIDO");
                print("Categoria:Adolecente");
                print("Valor:R$ 15,00");
                print("troco R$:%.2f"%troco);
+               quantJovens+=1;
                contLotacao+=1
                dinheiro+=15.00;
                jovens+=15.00;
-               quantJovens+1;
                #informativos
                print("\ningressos vendidos:%d"%contLotacao);
                print("Capacidade local:%d"%lotacaoMax);
@@ -73,24 +74,25 @@ while opcao!= "2": #laço principal de repetição do menu
               while uefs != "1" and uefs != "2":
                print("Desconto Estudantil para Universitario Uefs %:"); 
                uefs=input("\n[1]=sim % Não=[2] ");
-               input("\nPressione ENTER para confirmar....")
+               input("\nPressione ENTER para confirmar....");
                os.system("cls")
                match uefs:
                 case "1":#se for estudante
                  matricula=input("Matricula: ");#se sim aqui vem a matricula
 
                  if len(matricula) == 9 and matricula.isdigit():#aqui vejo requisitos matricula
+                  print("\nCATEGORIA:ESTUDANTE:PREÇO:R$20,00\n");
                   pagamento=float(input("Digite o valor pago $: "));
-                  if pagamento >=20:#aqui ainda verifico o valrdo pagamento
+                  if pagamento >=20:#aqui ainda verifico o valordo pagamento
                    troco =pagamento -20;
-                   print("INGRESSSO VENDIDO");
+                   print("\nINGRESSSO VENDIDO");
                    print("Categoria:Estudante");
                    print("Valor:R$ 20.00\n");  
                    print("Troco:R$ %.2f"%troco);
+                   quantEstudantes+=1
                    contLotacao+=1
                    dinheiro+=20.00;
                    universitystudent+=20.00
-                   quantEstudantes+=1;
                    print("\ningressos vendidos:%d"%contLotacao);
                    print("Capacidade local:%d"%lotacaoMax);
                   else:
@@ -99,7 +101,7 @@ while opcao!= "2": #laço principal de repetição do menu
                   print("Matricula invalida");#aqui ainda indico se matricula foi errada
                   
                 case"2":#se não for estudande
-                 print("Não é Estudante")
+                 print("\nCATEGORIA:ADULTO:PREÇO:R$40,00\n");
                  pagamento=float(input("Digite o valor pago $:"));
                  if pagamento >= 40.00:#validação pagamento menores que valor do 
                   troco = pagamento-40.00;
@@ -107,10 +109,10 @@ while opcao!= "2": #laço principal de repetição do menu
                   print("Categoria:Adulto");
                   print("Valor:R$ 40.00");
                   print("troco R$:%.2f"%troco);
+                  quantAdultos+=1;
                   contLotacao+=1;
                   dinheiro+=40.00;
                   adultos+=40.00;
-                  quantAdultos+=1;
                   print("\ningressos vendidos:%d"%contLotacao);
                   print("Capacidade local:%d"%lotacaoMax);
                #//////////////////////ADULTO////////////////////////////
@@ -133,7 +135,7 @@ while opcao!= "2": #laço principal de repetição do menu
     #premiação
 
 if dinheiro <=300.00:  
-   print("\o/ Premiaçoes do DIa \o/");
+   print("Premiaçoes do DIa ");
    premiacao = dinheiro*0.10;
    valor1 =premiacao*0.50;valor2=premiacao*0.30;valor3=premiacao*0.20
    print("1 lugar R$:%.2f\n2 lugar R$:%.2f\n 3 lugar R$:%.2f"%(valor1,valor2,valor3));
@@ -149,23 +151,24 @@ if dinheiro >=700.00:
    print("1 lugar R$:%.2f\n2 lugar R$:%.2f\n 3 lugar R$:%.2f"%(valor1,valor2,valor3));
     
     
-input("\nPressione ENTER para [DETALHES DO CAIXA$]...")
+input("\nPressione ENTER para >>>DETALHES DO CAIXA$<<<<")
 os.system("cls")
-print("$--------------[Relatorio]--------------$")
+print("$-----$--$-----$--[Relatorio]--$-----$--$-----$")
 print("\nDetalhamento Caixa");
-print("Categoria Total:\n");
+
+print("\nValores por Categoria");
 print("Jovens R$:%.2f.........."%jovens);
 print("Adultos R$:%.2f........."%adultos);
-print("Estudntes Uefs R$:%.2f.."%universitystudent)
-print("Não Pagantes:%d........."%kids);
+print("Estudantes Uefs R$:%.2f.."%universitystudent)
+print("Não Pagantes crianças:%d........."%kids);
 
-print("Ingressos vendido por Categoria")
+print("\nIngressos vendido por Categoria")
 print("Criança %d"%kids);
 print("Jovens %d"%quantJovens);
 print("Adultos %d"%quantAdultos);
 print("Estudantes %d"%quantEstudantes)
 
-print("\n\n\nTotal de Pagantes %d"%contLotacao);
+print("\n\nTotal de Pagantes %d"%contLotacao);
 print("Média de pagantes R$:%.2f"%(dinheiro/contLotacao));
 print("Total do dia R$:%.2f"%dinheiro);
 
